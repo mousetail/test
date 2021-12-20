317c73e1ea22c5c3c6f950750b9f7089de6cdd22
Maurits van Riezen (mousetail) <mousetail@mousetail.nl>
2021-12-20 22:02:53 +0100

    fit canvas to screen; add (static) paddles

--- public/js/pong.js
+++ public/js/pong.js
@@ -1,32 +1,50 @@
-app.onInit = function(){
-			this.nodes.push({
-				id : 'red-box',
-				x  : 100,
-				y  : 0,
-				width  : 100,
-				height : 100,
-				color  : 'red',
-				direction : 0
-			});
+app.onInit = function () {
+    app.width = window.innerWidth;
+    app.height = window.innerHeight;
 
-			this.nodes.push({
-				id : 'black-box',
-				x  : 50,
-				y  : 0,
-				width  : 150,
-				height : 150,
-				color  : 'black'
-			});
-		};
 
-		app.onUpdate = function(time){
-			this.getNode('black-box').y++;
+    document.querySelector('canvas').width = window.innerWidth;
+    document.querySelector('canvas').height = window.innerHeight;
 
-			if(Math.cos(this.timestamp / 100) > 0){
-				this.getNode('red-box').direction = -1;
-			}else{
-				this.getNode('red-box').direction = 1;
-			}
+    this.nodes.push({
+        id: 'left-paddle',
+        x: app.width / 30,
+        y: app.height / 2 - app.height / 8,
+        width: app.width / 40,
+        height: app.height / 4,
+        color: 'red',
+        direction: 0
+    });
 
-			this.getNode('red-box').x+=this.getNode('red-box').direction;
-		};
+    this.nodes.push({
+        id: 'right-paddle',
+        x: app.width - app.width / 30 - app.width / 40,
+        y: app.height / 2 - app.height / 8,
+        width: app.width / 40,
+        height: app.height / 4,
+        color: 'red',
+        direction: 0
+    })
+
+    this.nodes.push({
+        id: 'ball',
+        x: app.width / 2 - app.width/64,
+        y: app.height / 2 - app.height/64,
+        width: app.width/32,
+        height: app.width/32,
+        color: 'black'
+    });
+
+};
+
+app.onUpdate = function (time) {
+    this.getNode('black-box').y++;
+
+    if (Math.cos(this.timestamp / 100) > 0) {
+        this.getNode('red-box').direction = -1;
+    } else {
+        this.getNode('red-box').direction = 1;
+    }
+
+    this.getNode('red-box').x += this.getNode('red-box').direction;
+};

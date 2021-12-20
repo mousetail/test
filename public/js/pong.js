8d109040b3f0b79ec6f41057d0ef6d09afb295a8
Maurits van Riezen (mousetail) <mousetail@mousetail.nl>
2021-12-20 22:21:40 +0100

    add logic for paddles

--- public/js/pong.js
+++ public/js/pong.js
@@ -29,12 +29,12 @@ app.onInit = function () {
     let angle = Math.random() * 2 * Math.PI
     this.nodes.push({
         id: 'ball',
-        x: app.width / 2 - app.width/64,
-        y: app.height / 2 - app.height/64,
-        width: app.width/32,
-        height: app.width/32,
+        x: app.width / 2 - app.width / 64,
+        y: app.height / 2 - app.height / 64,
+        width: app.width / 32,
+        height: app.width / 32,
         color: 'black',
-        direction: [Math.sin(angle) * ball_speed * app.width / 1000, Math.cos(angle) * ball_speed * app.height /1000]
+        direction: [Math.sin(angle) * ball_speed * app.width / 1000, Math.cos(angle) * ball_speed * app.height / 1000]
     });
 
 };
@@ -46,10 +46,65 @@ app.onUpdate = function (time) {
         ball.y += ball.direction[1] * time;
 
         if (ball.y < 0 || ball.y > app.height) {
-            ball.direction[1]*=-1;
+            ball.direction[1] *= -1;
+            ball.y += ball.direction[1] * time;
         }
     }
 
-
+    let paddles = [this.getNode('left-paddle'), this.getNode('right-paddle')];
+    for (let paddle of paddles) {
+        if (paddle.up_pressed) {
+            paddle.y -= time * app.height * 0.0025;
+        }
+        if (paddle.down_pressed) {
+            paddle.y += time * app.height * 0.0025;
+        }
+    }
     //this.getNode('red-box').x += this.getNode('red-box').direction;
 };
+
+addEventListener('keydown', function (ev) {
+    if (ev.key === 'ArrowUp') {
+        app.getNode(
+            'right-paddle'
+        ).up_pressed = true;
+    }
+    if (ev.key === 'ArrowDown') {
+        app.getNode(
+            'right-paddle'
+        ).down_pressed = true;
+    }
+    if (ev.key === 'w') {
+        app.getNode(
+            'left-paddle'
+        ).up_pressed = true;
+    }
+    if (ev.key === 's') {
+        app.getNode(
+            'left-paddle'
+        ).down_pressed = true;
+    }
+});
+
+addEventListener('keyup', function (ev) {
+    if (ev.key === 'ArrowUp') {
+        app.getNode(
+            'right-paddle'
+        ).up_pressed = false;
+    }
+    if (ev.key === 'ArrowDown') {
+        app.getNode(
+            'right-paddle'
+        ).down_pressed = false;
+    }
+    if (ev.key === 'w') {
+        app.getNode(
+            'left-paddle'
+        ).up_pressed = false;
+    }
+    if (ev.key === 's') {
+        app.getNode(
+            'left-paddle'
+        ).down_pressed = false;
+    }
+})
